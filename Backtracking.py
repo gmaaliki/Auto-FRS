22d8e2b7dbfffe9e7496d3a5b836a5eb825a60ae
gmaaliki <maalikighifari@gmail.com>
2023-05-02 15:55:12 +0700

    baikin

--- Backtracking.py
+++ Backtracking.py
@@ -3,7 +3,7 @@ class Subject:
         self.name = attribute_array[0]
         self.subject_code = attribute_array[1]
         self.semester = attribute_array[2]
-        self.sks = attribute_array[3]
+        self.sks = int (attribute_array[3])
         self.day = attribute_array[4]
         self.start_hour = attribute_array[5]
         self.end_hour = attribute_array[6]
@@ -11,40 +11,65 @@ class Subject:
         self.lecturer = attribute_array[8]
 
 class Backtracking:
-    def __init__(self, input_file):
+    def __init__(self, input_file, user_semester):
         self.subject_arr = []
-        self.intervals = []
-        self.line_index = 0
-        self.sks_total = 0
+
+        # User inputs
+        self.user_semester = user_semester
 
         for line in input_file:
             self.attribute_array = line.split()
-            if self.attribute_array == '-':
+            if '-' in self.attribute_array:
                 continue
             self.subject = Subject(self.attribute_array)
             self.subject_arr.append(self.subject)
-            self.line_index += 1
     
     def find_combinations(self):
-        # self.backtrack([], self.nums, self.target)
-        # return self.results
-        print("hello")
+        self.intervals = []
+        self.path = []
+        self.results = []
+        self.sks_total = 0
+
+        self.backtrack()
+        self.printResults()
     
-    def backtrack(self, path, nums, target):
-        if target == 0:
-            # self.results.append(path)
+    def backtrack(self):
+        if self.sks_total > 18 and self.sks_total < 24:
+            if self.path:
+                self.results.append(self.path)
             return
-        if target < 0:
-            return
-        for i in range(len(nums)):
-            new_path = path + [nums[i]]
-            new_target = target - nums[i]
-            self.backtrack(new_path, nums[i:], new_target)
+
+        for a in self.subject_arr:
+            # Mencocokkan semester
+            if int(a.semester) != int(self.user_semester):
+                continue
+            # Memeriksa apakah mata kuliah sudah diambil atau tidak
+            for subj in self.path:
+                if a.name == subj.name:
+                    continue
+            
+            # Memeriksa apakah waktu sudah terpakai atau tidak
+            for times in self.intervals:
+                if (a.start_hour < times[1]) and (a.end_hour > times[0]):
+                    continue
+
+            self.path.append(a)
+            self.intervals.append((a.start_hour, a.end_hour))
+            self.sks_total += a.sks
+
+            self.backtrack()
+
+    def printResults(self):
+        for a in self.results:
+            for subj in a:
+                print(subj.name + subj.subject_code)
+        print("------------------------")
 
 def main():
     input_file = open('jadwal22-23.txt', 'r')
-    bt = Backtracking(input_file)
-    print(bt.subject_arr[5]. name)
+    user_semester = 4
+    bt = Backtracking(input_file, user_semester)
+    bt.find_combinations()
 
 if __name__ == "__main__":
     main()
